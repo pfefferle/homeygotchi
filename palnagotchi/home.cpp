2156bd951366d091625cd76b20595cecfe73dac1
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-04-21 22:18:21 +0200

    coding stye

--- palnagotchi/home.cpp
+++ palnagotchi/home.cpp
@@ -2,31 +2,40 @@
 
 BTHome bthome;
 
+uint32_t last_update = 10001;
+
 void initHome() {
     // Initialize BTHome with device name.
-    String deviceName = homeygotchi_name;
-    bthome.begin(deviceName, false, "", false);
+    String device_name = homeygotchi_name;
+    bthome.begin(device_name, false, "", false);
+
+    // Send initial update.
+    updateHome(true);
 }
 
-void updateHome() {
-    // Reset previous measurements
+void updateHome(bool force) {
+    // Update only every 10 minutes.
+    if (!force && millis() - last_update < 600000) {
+        return;
+    }
+    last_update = millis();
+
+    // Reset previous measurements.
     bthome.resetMeasurement();
 
-    // Add current face as a text measurement
+    // Add current face as a text measurement.
     String face = getCurrentMoodFace();
-    uint8_t faceBytes[face.length() + 1];
-    face.getBytes(faceBytes, face.length() + 1);
-    bthome.addMeasurement(ID_TEXT, faceBytes, face.length() + 1);
+    uint8_t face_bytes[face.length() + 1];
+    face.getBytes(face_bytes, face.length() + 1);
+    bthome.addMeasurement(ID_TEXT, face_bytes, face.length() + 1);
 
-    // Add current mood as a text measurement
+    // Add current mood as a text measurement.
     String phrase = getCurrentMoodPhrase();
-    uint8_t phraseBytes[phrase.length() + 1];
-    phrase.getBytes(phraseBytes, phrase.length() + 1);
-    bthome.addMeasurement(ID_TEXT, phraseBytes, phrase.length() + 1);
+    uint8_t phrase_bytes[phrase.length() + 1];
+    phrase.getBytes(phrase_bytes, phrase.length() + 1);
+    bthome.addMeasurement(ID_TEXT, phrase_bytes, phrase.length() + 1);
 
-    bthome.buildPaket();
-
-    // Send the packet
+    // Send the packet.
     bthome.sendPacket();
 
     bthome.stop();
